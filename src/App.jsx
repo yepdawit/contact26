8298a56851ea763ae289fc4a22f3f717f2be1c68
Dawit Gashaw <yepdawit@gmail.com>
2023-07-24 14:25:23 -0400

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React from "react";
+import ContactList from "./components/ContactList";
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+export default function App() {
+  return (
+    <>
+      <ContactList />
+    </>
+  );
 }
-
-export default App;
---
+++ src/components/ContactRow.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function COntactRow({ contact }) {
+  return (
+    <tr>
+      <td>{contact.name}</td>
+      <td>{contact.email}</td>
+      <td>{contact.phone}</td>
+    </tr>
+  );
+}
